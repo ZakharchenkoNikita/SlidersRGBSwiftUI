e93d63e09c999541d473e2eebc1121db4938dc50
immortalprogrammist <immortalvelial@icloud.com>
2021-09-10 09:43:34 +0200

    Initial Commit

---
+++ SlidersRGBSwiftUI/SlidersRGBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersRGBSwiftUIApp.swift
+//  SlidersRGBSwiftUI
+//
+//  Created by Nikita on 10.09.21.
+//
+
+import SwiftUI
+
+@main
+struct SlidersRGBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
